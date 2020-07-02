d874ca4ad99673b006f01c82fead5f3c266d4b38
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-07-02 19:20:18 -0300

    date

--- classes/Utils.js
+++ classes/Utils.js
@@ -1,5 +1,5 @@
 class Utils {
     static dateFormat(date) {
-        return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+''+date.getHours()+':'+date.getMinutes();
+        return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes();
     }
 }
