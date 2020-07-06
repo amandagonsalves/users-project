e475d7fdca2aad8a8ce1e6b3bf49a00977402f0b
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-07-06 14:37:57 -0300

    profile js

---
+++ profile.js
@@ -0,0 +1,37 @@
+const characters = [
+    {
+        name: 'Jonathan',
+        imgProfile: '/img/user1-128x128.jpg'
+    },
+    {
+        name: 'Maria',
+        imgProfile: '/img/user4-128x128.jpg'
+    },
+    {
+        name: 'Lucas',
+        imgProfile: '/img/user6-128x128.jpg'
+    }
+]
+function renderCard(character) {
+    let pub = document.querySelector('.activity-pub');
+    let div = document.createElement('div');
+    div.innerHTML = `
+        <div id="profile-head">
+            <img src="${character.imgProfile}" class="img-circle"/>
+            <p>${character.name}</p>
+        </div>
+        Lorem ipsum represents a long-held tradition for designers, typographers and the like. Some people hate it and argue for its demise, but others ignore the hate as they create awesome tools to help create filler text for everyone from bacon lovers to Charlie Sheen fans.
+        <div id="icons-activity">
+            <i class="fa fa-share"></i>
+            <i class="fa fa-like"></i>
+            <i class="fa fa-comment"></i>
+        </div>
+    `
+    pub.appendChild(div);
+}
+characters.forEach(character => {
+    renderCard(character);
+}); 
+document.querySelector('.btn-follow').addEventListener('click', e => {
+    console.log('ola')
+})
