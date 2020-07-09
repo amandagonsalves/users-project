9ec2da3897608e9a4105569f34572481332fd902
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-07-09 20:09:18 -0300

    profile

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -59,10 +59,6 @@ class ProfileController {
         let profile = {};
         let isValid = true;
         [...this.formSettings.elements].forEach((field,index) => {
-            if (['nameAbout', 'emailAbout', 'experienceAbout'].indexOf(field.name) > -1 && !field.value) {
-                field.parentElement.classList.add('has-error');
-                isValid = false
-            }
             if(field.name === 'agree') {
                 profile[field.name] = field.checked;
                 if(field.checked === false) {
